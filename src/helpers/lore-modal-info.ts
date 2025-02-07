b8755afa6c7366d24f9f6658b67ee078fd00faad
Gabriela Quintanilla <gabyquin14@gmail.com>
2025-02-07 11:57:10 -0500

    added responsive of map modal

--- src/helpers/lore-modal-info.ts
+++ src/helpers/lore-modal-info.ts
@@ -19,14 +19,14 @@ export const loreModalInfo: ModalInfoInterface[] = [
     title: "Skyline District",
     link: "https://www.youtube.com/watch?v=CXziasLolDg",
     desc: `After the incident at AICANDII LABS, Buffpup got lucky with a lawsuit that awarded her damages to her epic jacket and lungs, not to mention emotional damages. All the goo in the black lake caused lasting damage…
-      <br/> And massive profits! <br/> The payout allowed Buff to buy a skyscraper in the Skyline District, where the pups can do all of their corpa things, like play with papers and drink coffee. Buff isn't exactly sure what happens within the building. There’s an arrow that’s sometimes green and sometimes red. That’s about as much as she’s gleaned from the screen in her office, but she’ll get back to you on it.`,
+      <br/><br/> And massive profits!<br/> <br/> The payout allowed Buff to buy a skyscraper in the Skyline District, where the pups can do all of their corpa things, like play with papers and drink coffee. Buff isn't exactly sure what happens within the building. There’s an arrow that’s sometimes green and sometimes red. That’s about as much as she’s gleaned from the screen in her office, but she’ll get back to you on it.`,
     img: "https://framerusercontent.com/images/4NPPFColCHBnyR7MhOxIV79tGV8.jpg",
     styles: "skyline",
   },
   {
     title: "Silver Creek",
     desc: `Silver Creek is a suburban development settled beside the creek it was named for. Rows of charming houses with well-manicured lawns line the peaceful streets, each neatly matched with the one next to it. Beautiful flowers pepper the yards, and children play in the streets. If asked to pick out a house from the rest, one would struggle to decide—until a loud explosion or a distinct cloud of smoke emerges from one particular garage.
-      <br/>Tucked between the cookie-cutter homes stands a house—much like the rest—only remarkable if a mistake is made. It is the hackneyed home base of an unhinged scientist, externally unremarkable but filled to the brim with out-of-this-world technology. The space has been transformed into a laboratory.`,
+      <br/><br/>Tucked between the cookie-cutter homes stands a house—much like the rest—only remarkable if a mistake is made. It is the hackneyed home base of an unhinged scientist, externally unremarkable but filled to the brim with out-of-this-world technology. The space has been transformed into a laboratory.`,
     styles: "silver",
   },
   {
@@ -46,7 +46,7 @@ export const loreModalInfo: ModalInfoInterface[] = [
     title: "Doodle Dash",
     link: "https://www.youtube.com/watch?v=4CxlS6IYnFM",
     desc: `A bright arcade sits between a cafe and a stuffed toy store. The façade stands out due to the cat girl motifs in neon. This arcade was ‘lovingly adopted’ by a mischievous sprout cat girl. If you pry, the cat girl will promise she only works there, though it’s hard to believe her as she always winks as she says it.
-      <br/>Once inside the arcade, the atmosphere transforms into decadence and bright lights. Rows of arcade cabinets, their glowing screens offering a kaleidoscope of scores and buttons to toy with. Crane machines stand along every exposed space, and gachapon machines are in every corner. Little honeybuns tend to the machines, eager to ensure everyone has the best time possible.`,
+      <br/><br/>Once inside the arcade, the atmosphere transforms into decadence and bright lights. Rows of arcade cabinets, their glowing screens offering a kaleidoscope of scores and buttons to toy with. Crane machines stand along every exposed space, and gachapon machines are in every corner. Little honeybuns tend to the machines, eager to ensure everyone has the best time possible.`,
     img: "https://framerusercontent.com/images/xzRPLXELOrYiAmYMOlkSFSOin0.png",
     styles: "doodle",
   },
@@ -74,21 +74,21 @@ export const loreModalInfo: ModalInfoInterface[] = [
     title: "Blackthorn Mire",
     link: "https://www.youtube.com/watch?v=23fTS2kGD3s",
     desc: `AICANDII was forced to leave Silver Creek after too many complaints from the HOA and because the fumes were beginning to become a problem for the area's families. Candii still isn't sure what all the fuss was about; it was negligible.
-      <br/>Regardless, Candii moved to an abandoned facility in Blackthorn Mire, where an alternate version of themselves had previously run. It's a long story, with portals and everything. Just know that Candii dimension-hopped, and that's something they can do. Great. Get out of this file section; I'm sure it's too much for you to understand.`,
+      <br/><br/>Regardless, Candii moved to an abandoned facility in Blackthorn Mire, where an alternate version of themselves had previously run. It's a long story, with portals and everything. Just know that Candii dimension-hopped, and that's something they can do. Great. Get out of this file section; I'm sure it's too much for you to understand.`,
     img: "https://framerusercontent.com/images/CrGRw7DCzq8vbu5a72TOs9DQhGI.png",
     styles: "blackthorn",
   },
   {
     title: "Mourningwood",
     desc: `The quiet of the forest twists as night descends. Desperate howls and whimpers rise through the misty air as creatures awaken. Whispers of ancient folklore feel real now as the stories of werewolves rise within the mind. The occasional hoot of an owl or the rustle of a small creature scurrying through the underbrush only adds to the place’s mystique.
-      <br/>Shadows elongate and intertwine, the distant flashes of orange and tan caught between the dense brush. The glint of blue behind a tree’s silhouette, claws drawn along fallen logs. A mischievous little laugh catches your ear, an invitation to play — or to run.`,
+      <br/><br/>Shadows elongate and intertwine, the distant flashes of orange and tan caught between the dense brush. The glint of blue behind a tree’s silhouette, claws drawn along fallen logs. A mischievous little laugh catches your ear, an invitation to play — or to run.`,
     styles: "mourningwood",
   },
   {
     title: "Pet Cemetery",
     desc: `A well-lived life is precious in its own right. And this particular life was precious, as all pet cats are to their owners. But then rosedoodle became sick, despite the best efforts of their owner. They tried everything to take care of their darling cat, to see her play and enjoy herself again. Rosedoodle tried her best until the very end to keep a smile on their owner's face.
-      <br/>After saying goodbye to rosedoodle as she passed away in their home, they sought to bury her in the nicest place they could think of. However, when her owner placed the much-beloved cat into its final resting place, they left, thinking she would remain there forever. No one would suspect this was the beginning of Rose's next life.
-      <br/>Thanks to a combination of a bubbly pink flower and a Honeybun named Beepu, rosedoodle returned from her grave a newly formed cat sprout, partially human, partially cat, all parasitic—erm, all cute, that is. While Rose no longer lives at the pet cemetery, she took one of the flowers with her to help spread joy in Stardust City.`,
+      <br/><br/>After saying goodbye to rosedoodle as she passed away in their home, they sought to bury her in the nicest place they could think of. However, when her owner placed the much-beloved cat into its final resting place, they left, thinking she would remain there forever. No one would suspect this was the beginning of Rose's next life.
+      <br/><br/>Thanks to a combination of a bubbly pink flower and a Honeybun named Beepu, rosedoodle returned from her grave a newly formed cat sprout, partially human, partially cat, all parasitic—erm, all cute, that is. While Rose no longer lives at the pet cemetery, she took one of the flowers with her to help spread joy in Stardust City.`,
     styles: "pet",
   },
   {
@@ -101,7 +101,6 @@ export const loreModalInfo: ModalInfoInterface[] = [
     title: "Summer Adventure",
     link: "https://www.youtube.com/watch?v=94BeNyD5_Mc",
     desc: "What better time of year to get in bikinis and catch some rays than summertime? VchiBan made the most of the warm weather with a fun beach trip, games, and a campfire to round out the evening.",
-    img: "https://framerusercontent.com/images/DnbqrbuD0d83kwNlpxaTx7vTtkY.png",
     styles: "summer",
   },
 ];
