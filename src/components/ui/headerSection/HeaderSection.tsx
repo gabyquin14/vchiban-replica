e4bf856182d9f7fea6088f4dbff415d38f2ce6ad
Gabriela Quintanilla <gabyquin14@gmail.com>
2025-02-12 19:39:23 -0500

    finished responsive styles in game corner page

--- src/components/ui/headerSection/HeaderSection.tsx
+++ src/components/ui/headerSection/HeaderSection.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./HeaderSection.scss";
 
 interface HeaderSectionProps {
-  as?: keyof JSX.IntrinsicElements; // Allows to select the type of thml tag
+  as?: keyof JSX.IntrinsicElements; // Allows to select the type of html tag
   title: string;
   preDescription?: string;
   description?: React.ReactNode;
   headingLevel?: keyof JSX.IntrinsicElements;
   SparklesComponent?: React.ElementType;
   customStyles?: string;
+  children?: JSX.Element | JSX.Element[];
 }
 
 const HeaderSection: React.FC<HeaderSectionProps> = ({
@@ -19,9 +20,11 @@ const HeaderSection: React.FC<HeaderSectionProps> = ({
   headingLevel: Heading = "h1",
   SparklesComponent,
   customStyles = "",
+  children,
 }) => (
   <Component className={`header-section ${customStyles}`}>
     <div className="title-wrapper">
+      {<>{children}</>}
       <Heading className="title" dangerouslySetInnerHTML={{ __html: title }} />
       {SparklesComponent && <SparklesComponent className="sparkles" />}
     </div>
